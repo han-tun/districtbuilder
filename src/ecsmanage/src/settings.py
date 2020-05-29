251596b155d7cc6494b5aa3b66000d6cbb5e4513
Cole Kettler <me@colekettler.com>
2020-05-29 12:06:33 -0400

    Port django-ecsmanage to run ECS tasks

---
+++ src/ecsmanage/src/settings.py
@@ -0,0 +1,18 @@
+ECSMANAGE_ENVIRONMENTS = {
+    'default': {
+        'TASK_DEFINITION_NAME': 'StagingApp',
+        'CLUSTER_NAME': 'ecsStagingCluster',
+        'LAUNCH_TYPE': 'FARGATE',
+        'SECURITY_GROUP_TAGS': {
+            'Name': 'sgStagingAppEcsService',
+            'Environment': 'Staging',
+            'Project': 'DistrictBuilder'
+        },
+        'SUBNET_TAGS': {
+            'Name': 'PrivateSubnet',
+            'Environment': 'Staging',
+            'Project': 'DistrictBuilder'
+        },
+        'AWS_REGION': 'us-east-1',
+    },
+}
